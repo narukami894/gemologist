38ec64e2218c7a2e76a70b8d7425997019d28025
narukami894 <s.yakushiji@icloud.com>
2017-03-05 00:31:55 +0900

    add columns

--- db/migrate/20170304152553_add_some_columns.rb
+++ db/migrate/20170304152553_add_some_columns.rb
@@ -1,7 +1,8 @@
-class AddOmniauthColumnsToDevelopers < ActiveRecord::Migration[5.0]
+class AddSomeColumns < ActiveRecord::Migration[5.0]
   def change
     add_column :developers, :uid,      :string, null: false, default: ''
     add_column :developers, :provider, :string, null: false, default: ''
+    add_column :developers, :image,    :string
     add_index  :developers, [:uid, :provider], unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304130653) do
+ActiveRecord::Schema.define(version: 20170304152553) do
 
   create_table "developer_teams", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "developer_id"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20170304130653) do
     t.datetime "updated_at",                          null: false
     t.string   "uid",                    default: "", null: false
     t.string   "provider",               default: "", null: false
+    t.string   "image"
     t.index ["email"], name: "index_developers_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_developers_on_reset_password_token", unique: true, using: :btree
     t.index ["uid", "provider"], name: "index_developers_on_uid_and_provider", unique: true, using: :btree
